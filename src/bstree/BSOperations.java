49c5cfb1fae7b2a7b6b476481936388e1f48d51c
vinit <vinitsxn@gmail.com>
2022-01-10 16:39:41 +0530

    Added: Welcome message added

---
+++ src/bstree/BSOperations.java
@@ -0,0 +1,16 @@
+/**
+ * @author Vinit Kumar
+ * @created on 10-01-2022
+ * Binary Search Tree operations
+ */
+package bstree;
+
+/**
+ * calls operations for binary search tree
+ */
+public class BSOperations {
+
+    public static void main(String[] args) {
+        System.out.println("Binary search tree operations");
+    }
+}
